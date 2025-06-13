eed796fd20c6bdfa8e9fb586ab2cf29ec117befe
n-speech <dafna5566@gmail.com>
2025-06-13 19:01:32 +0300

    Update db.js

--- db.js
+++ db.js
@@ -3,6 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { Pool } = pkg;
+
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
